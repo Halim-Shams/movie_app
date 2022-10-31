297cc374c3734765afd98959d662efcd7beb2b3c
nishtha53 <68805896+nishtha53@users.noreply.github.com>
2022-10-31 13:16:19 +0530

    feat: search icon added

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { FaSearch } from "react-icons/fa";
+
 
 function Search() {
 	return (
 		<div className='flex items-center justify-center w-full'>
 			<div className='bg-white flex items-center justify-end shadow-md px-1.5 w-8/12 h-11 rounded-md'>
+			<div style={{"float": "left"}}>	
+			<FaSearch / >
+			</div>
 				<input
 					className='w-11/12 text-lg font-medium text-gray-900 outline-none placeholder:font-thin'
 					type='search'
