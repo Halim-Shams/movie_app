626b4c82028049d17d59ae68e706f1c0ae1a3d48
nishtha53 <68805896+nishtha53@users.noreply.github.com>
2022-10-31 14:54:28 +0530

    fix: space and icon size issue

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -6,7 +6,7 @@ function Search() {
 	return (
 		<div className='flex items-center justify-center w-full'>
 			<div className='bg-white flex items-center justify-end shadow-md px-1.5 w-8/12 h-11 rounded-md'>
-			<div>	
+			<div style={{"padding": "15px", "fontSize": "20px"}}>	
 			<FaSearch / >
 			</div>
 				<input
